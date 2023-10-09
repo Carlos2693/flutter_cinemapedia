7ffd2718adb412bfe080225bc8469094d8e39f3d
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-10-09 17:01:43 -0600

    Full App - Cinemapedia

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDbToEntity(MovieMovieDB movieDb) => Movie(
+        adult: movieDb.adult,
+        backdropPath: (movieDb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDb.backdropPath}'
+          : 'https://ih1.redbubble.net/image.1027712254.9762/pp,840x830-pad,1000x1000,f8f8f8.u2.jpg',
+        genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+        id: movieDb.id,
+        originalLanguage: movieDb.originalLanguage,
+        originalTitle: movieDb.originalTitle,
+        overview: movieDb.overview,
+        popularity: movieDb.popularity,
+        posterPath: (movieDb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
+          : 'no-poster',
+        releaseDate: movieDb.releaseDate,
+        title: movieDb.title,
+        video: movieDb.video,
+        voteAverage: movieDb.voteAverage,
+        voteCount: movieDb.voteCount,
+      );
+}
---
+++ lib/presentation/provider/provider.dart
@@ -0,0 +1,2 @@
+export 'movie/movies_providers.dart';
+export 'movie/movie_repository_provider.dart';
