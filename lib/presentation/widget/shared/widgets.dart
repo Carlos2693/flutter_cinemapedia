7c83d1170620b9f16b7778038ea5dfcc3e38e968
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-11-09 06:47:17 -0600

    custom bottom navigation

--- lib/presentation/widget/shared/widgets.dart
+++ lib/presentation/widget/shared/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:cinemapedia/presentation/widget/movie/movie_slideshow.dart';
 export 'package:cinemapedia/presentation/widget/shared/custom_appbar.dart';
+export 'package:cinemapedia/presentation/widget/shared/custom_bottom_navigation.dart';
