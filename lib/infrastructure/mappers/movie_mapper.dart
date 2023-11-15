36357ffe28e403d2cb46f9f5cefa341fc9f6ef20
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-11-15 09:57:03 -0600

    MovieDB to Entity

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -22,4 +23,25 @@ class MovieMapper {
         voteAverage: movieDb.voteAverage,
         voteCount: movieDb.voteCount,
       );
+
+  static Movie movieDetailsToEntity(MovieDetails movieDb) => Movie(
+    adult: movieDb.adult,
+    backdropPath: (movieDb.backdropPath != '')
+      ? 'https://image.tmdb.org/t/p/w500${movieDb.backdropPath}'
+      : 'https://ih1.redbubble.net/image.1027712254.9762/pp,840x830-pad,1000x1000,f8f8f8.u2.jpg',
+    genreIds: movieDb.genres.map((e) => e.name).toList(),
+    id: movieDb.id,
+    originalLanguage: movieDb.originalLanguage,
+    originalTitle: movieDb.originalTitle,
+    overview: movieDb.overview,
+    popularity: movieDb.popularity,
+    posterPath: (movieDb.posterPath != '')
+      ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
+      : 'https://ih1.redbubble.net/image.1027712254.9762/pp,840x830-pad,1000x1000,f8f8f8.u2.jpg',
+    releaseDate: movieDb.releaseDate,
+    title: movieDb.title,
+    video: movieDb.video,
+    voteAverage: movieDb.voteAverage,
+    voteCount: movieDb.voteCount,
+  );
 }
