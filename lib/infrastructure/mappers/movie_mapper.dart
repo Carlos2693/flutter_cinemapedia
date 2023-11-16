eb7037babb9ddcdeb803d79f757bcd320267bc6e
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-11-16 08:16:47 -0600

    Mostrar peliculas en la busqueda

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,8 +16,8 @@ class MovieMapper {
         popularity: movieDb.popularity,
         posterPath: (movieDb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
-          : 'no-poster',
-        releaseDate: movieDb.releaseDate,
+          : 'https://i.kym-cdn.com/entries/icons/facebook/000/018/587/tumblr_m99c8pVhtA1reb1c2o1_400.jpg',
+        releaseDate: movieDb.releaseDate ?? DateTime.now(),
         title: movieDb.title,
         video: movieDb.video,
         voteAverage: movieDb.voteAverage,
