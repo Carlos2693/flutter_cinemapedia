663f992be4abaf3f56e84ea213464c780a0f4c3e
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-12-06 14:28:53 -0600

    Resolución de la tarea - InfiniteScroll

--- lib/presentation/view/movie/favorites_view.dart
+++ lib/presentation/view/movie/favorites_view.dart
@@ -17,11 +17,26 @@ class FavoritesView extends ConsumerStatefulWidget {
 
 class FavoritesViewState extends ConsumerState<FavoritesView> {
 
+  bool isLastPage = false;
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
 
-    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+    if (isLoading || isLastPage) return;
+    isLoading = true;
+
+    final movies = await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    isLoading = false;
+
+    if (movies.isEmpty) {
+      isLastPage = false;
+    }
   }
 
   @override
@@ -30,7 +45,10 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
 
     return Scaffold(
-      body: MovieMasonry(movies: favoriteMovies),
+      body: MovieMasonry(
+        loadNextPage: loadNextPage,
+        movies: favoriteMovies
+      ),
     );
   }
 }
